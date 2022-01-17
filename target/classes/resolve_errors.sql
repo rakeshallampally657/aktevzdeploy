27af13a7b8272d3b7fc81764f02ed6b398a37f18
rakeshallampally657 <79206625+rakeshallampally657@users.noreply.github.com>
2022-01-17 12:38:51 +0100

    try

---
+++ target/classes/resolve_errors.sql
@@ -0,0 +1,4 @@
+alter table db_aktenvz.akte drop column stadtbezirk;
+alter table db_aktenvz.akte drop column almosenkasten;
+alter table db_aktenvz.akte drop column kennziffer;
+alter table db_aktenvz.grundstuecksbezeichnung drop column flurstueck;
